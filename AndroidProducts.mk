1902c0676f272f4c0ba0430b5610b726b3170de3
Minionguyjpro <minionguyjpro@gmail.com>
2024-07-30 20:47:45 +0200

    Add TWRP device tree part
    Signed-off-by: Minionguyjpro <minionguyjpro@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Tab11WiFi_NEU.mk
+    $(LOCAL_DIR)/lineage_Tab11WiFi_NEU.mk \
+    $(LOCAL_DIR)/twrp_Tab11WiFi_NEU.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_Tab11WiFi_NEU-user \
     lineage_Tab11WiFi_NEU-userdebug \
-    lineage_Tab11WiFi_NEU-eng
+    lineage_Tab11WiFi_NEU-eng \
+    twrp_Tab11WiFi_NEU-eng \
