88131cb4ded5bab0cb7d977347124fd6cd88ffdc
Minionguyjpro <minionguyjpro@gmail.com>
2024-07-31 09:48:58 +0000

    Update device tree properly
    Signed-off-by: Minionguyjpro <minionguyjpro@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,15 +1,19 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright 2024 The Android Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Tab11WiFi_NEU.mk \
-    $(LOCAL_DIR)/twrp_Tab11WiFi_NEU.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_Tab11WiFi_NEU.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_Tab11WiFi_NEU-user \
-    lineage_Tab11WiFi_NEU-userdebug \
-    lineage_Tab11WiFi_NEU-eng \
-    twrp_Tab11WiFi_NEU-eng \
+COMMON_LUNCH_CHOICES := twrp_Tab11WiFi_NEU-eng
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,20 +1,34 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright 2024 The Android Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
 
 DEVICE_PATH := device/incar/Tab11WiFi_NEU
 
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    vendor \
-    vbmeta_system \
-    vbmeta_vendor \
-    boot \
-    system \
-    product
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# The board uses the recovery in the boot image and partition
 BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Architecture
@@ -32,43 +46,55 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
-# APEX
-OVERRIDE_TARGET_FLATTEN_APEX := true
+TARGET_BOARD_PLATFORM := mt6771
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Tab 11 WiFi
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := tb8788p1_64_bsp_k419
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
-# Display
-TARGET_SCREEN_DENSITY := 240
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_USES_MTK_HARDWARE := true
+MTK_HARDWARE := true
+
+# Platform
+BOARD_VENDOR := mtk
+TARGET_BOARD_PLATFORM := mt6771
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Kernel
-BOARD_BOOTIMG_HEADER_VERSION := 2
-BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 buildvariant=user
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_RAMDISK_OFFSET := 0x14f88000
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_TAGS_OFFSET := 0x13f88000
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x14f88000
+BOARD_DTB_OFFSET := 0x13f88000
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_CONFIG := Tab11WiFi_NEU_defconfig
-TARGET_KERNEL_SOURCE := kernel/incar/Tab11WiFi_NEU
-
-# Kernel - prebuilt
-TARGET_FORCE_PREBUILT_KERNEL := true
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-BOARD_INCLUDE_DTB_IN_BOOTIMG := 
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
-BOARD_KERNEL_SEPARATED_DTBO := 
-endif
+# BOARD_KERNEL_SEPARATED_DTBO := true
+# TARGET_KERNEL_CONFIG := Tab11WiFi_NEU_defconfig
+# TARGET_KERNEL_SOURCE := kernel/incar/Tab11WiFi_NEU
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
@@ -82,8 +108,15 @@ BOARD_INCAR_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     system
 BOARD_INCAR_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 
-# Platform
-TARGET_BOARD_PLATFORM := mt6771
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USES_MKE2FS := true
+
+# Encryption
+TW_INCLUDE_CRYPTO := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
@@ -94,30 +127,36 @@ TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_ODM_DLKM_PROP += $(DEVICE_PATH)/odm_dlkm.prop
 TARGET_VENDOR_DLKM_PROP += $(DEVICE_PATH)/vendor_dlkm.prop
 
-# Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6771
-TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
 # Security patch level
 VENDOR_SECURITY_PATCH := 2023-06-05
 
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+
 # Verified Boot
-BOARD_AVB_ENABLE := true
+BOARD_AVB_ENABLE := false
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 
-# VINTF
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+# Debug
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+TWRP_EVENT_LOGGING := false
+#TARGET_RECOVERY_DEVICE_MODULES += debuggerd
+#TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT_EXECUTABLES)/debuggerd
 
-# Inherit the proprietary files
-include vendor/incar/Tab11WiFi_NEU/BoardConfigVendor.mk
-
-# TWRP Configuration
+# TWRP specific build flags
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 150
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INCLUDE_NTFS_3G := true
+TW_EXTRA_LANGUAGES := true
+TW_USE_EXTERNAL_STORAGE := true
+TW_USE_NEW_MINADBD := true
 TW_USE_TOOLBOX := true
-TW_INCLUDE_REPACKTOOLS := true
+TW_NO_LEGACY_PROPS := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_DEVICE_VERSION := 0
