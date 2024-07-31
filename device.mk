1cdbb828d387c86c785d9386db9bfaab3c9c4842
Minionguyjpro <minionguyjpro@gmail.com>
2024-07-31 15:55:26 +0000

    Extend recovery ramdisk structure
    Signed-off-by: Minionguyjpro <minionguyjpro@gmail.com>

--- device.mk
+++ device.mk
@@ -1,6 +1,24 @@
-LOCAL_PATH := android_device_incar_Tab11WiFi_NEU
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Dynamic Partitions
+DEVICE_PATH := device/incar/Tab11WiFi_NEU
+PRODUCT_CHARACTERISTICS := tablet
+
+# Dynamic Partitions stuff
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # VNDK
@@ -10,6 +28,7 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 PRODUCT_PLATFORM := mt6771
+#PRODUCT_PLATFORM := mt8788
 
 # A/B
 ENABLE_VIRTUAL_AB := true
@@ -27,16 +46,23 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
+    update_engine_client \
+    bootctrl
+    
 PRODUCT_PACKAGES += \
     otapreopt_script \
     update_engine \
     update_verifier \
     update_engine_sideload
-    
+
+# MTK PlPath Utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils.recovery
+
+# Fastbootd stuff
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
 # health Hal
@@ -45,4 +71,4 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
 
 TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.product.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
--- twrp_Tab11WiFi_NEU.mk
+++ twrp_Tab11WiFi_NEU.mk
@@ -13,19 +13,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+DEVICE_PATH := device/incar/Tab11WiFi_NEU
 
 # Release name
 PRODUCT_RELEASE_NAME := Tab11WiFi_NEU
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Installs GSI keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from device
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
-PRODUCT_MODEL := Tab11WiFi_NEU
+PRODUCT_MODEL := Tab 11 WiFi
 PRODUCT_BRAND := Blackview
 PRODUCT_MANUFACTURER := incar
+
+PRODUCT_GMS_CLIENTID_BASE := android-incar
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=Tab11WiFi_NEU \
+    PRODUCT_NAME=Tab11WiFi_NEU \
+    PRIVATE_BUILD_DESC="full_tb8788p1_64_bsp_k419-user 12 Tab11WiFi_NEU 20230801.1751 release-keys"
+
+BUILD_FINGERPRINT := Blackview/Tab11WiFi_NEU/Tab11WiFi_NEU:12/Tab11WiFi_NEU/20230801.1751:user/release-keys
